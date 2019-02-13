91a12f51e2afe4e20e35d13398a12c19645c7ccf
David <phone.email459@gmail.com>
2019-02-13 09:53:05 -0600

    Customizing GUI
    Make more usable to our needs, etc.

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+			javac gui.java
+			java gui
--- gui.java
+++ gui.java
@@ -6,40 +6,31 @@ class gui {
     public static void main(String args[]) {
 
         //Creating the Frame
-        JFrame frame = new JFrame("Chat Frame");
+        JFrame frame = new JFrame("Easy One Click Stress Test");
+        ImageIcon img = new ImageIcon("pic.jpg");
+        frame.setIconImage(img.getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
+        frame.setSize(350, 300);
 
         //Creating the MenuBar and adding components
         JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("FILE");
-        JMenu m2 = new JMenu("Help");
+        JMenu m1 = new JMenu("Help");
         mb.add(m1);
-        mb.add(m2);
-        JMenuItem m11 = new JMenuItem("Open");
-        JMenuItem m22 = new JMenuItem("Save as");
-        m1.add(m11);
-        m1.add(m22);
 
         //Creating the panel at bottom and adding components
         JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter Text");
-        JTextField tf = new JTextField(10); // accepts upto 10 characters
-        JButton send = new JButton("Send");
-        JButton reset = new JButton("Reset");
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(send);
-        panel.add(reset);
+        JButton start = new JButton("Start"); // TODO(David): Make sure the file path is set
+                                              //              correctly.
 
-        // Text Area at the Center
-        JTextArea ta = new JTextArea();
+        JButton stop = new JButton("Stop");   // TODO(David): Make a pop up window for
+                                              //              "Are you sure you want to stop?"
+        panel.add(start);
+        panel.add(stop);
 
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.WEST, panel);
         frame.getContentPane().add(BorderLayout.NORTH, mb);
-        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
