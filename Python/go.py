1f2b52afe8dee573a4e51d3f0fe79919beb9ff6e
David <phone.email459@gmail.com>
2019-02-20 23:00:31 -0600

    More functionallity

--- Python/go.py
+++ Python/go.py
@@ -2,6 +2,7 @@ from out import write
 from runner import Runner
 from datetime import date
 from os.path import isfile
+from datetime import datetime
 
 '''
 CSV file output is such:
@@ -12,14 +13,25 @@ CSV file output is such:
 ]
 '''
 
+def calc_days():
+	f = open("Python/start.txt", 'r')
+	lst = f.readlines()
+	f.close()
+	arr = [int(x.strip()) for x in lst]
+	d0 = date(arr[0], arr[1], arr[2])
+	curr_year = datetime.now().year
+	curr_mon  = datetime.now().month
+	curr_day  = datetime.now().day
+	d1 = date(curr_year, curr_mon, curr_day)
+
+	delta = d1 - d0
+	return delta.days
+
+DAYS = calc_days() # TODO (ALL): CHANGE START FILE TO DATE THAT YOU START
+
 def main():
-	pass
 	# Run the test
 	# make a csv file
-	# make a hidden file so that we know the start date.
-	# parse the date so that it is easy to determine days
-	# https://stackoverflow.com/questions/151199/how-to-calculate-number-of-days-between-two-given-dates
-	# 
 
 if __name__ == '__main__':
 	main()
