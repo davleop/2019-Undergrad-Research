12a8b4206ed6eea0f5ed7bd6602fb21769f53578
davleop <dlpenn1@buff.wtamu.edu>
2019-03-06 22:08:51 -0600

    Fixed timer

--- StressTester.java
+++ StressTester.java
@@ -19,7 +19,7 @@ class StressTester extends JFrame implements ActionListener {
     private static String OS = System.getProperty("os.name").toLowerCase();
     private final static String file_to_run   = "Python/go.py";
     private final static String clean_up_file = "Python/cleanup.py";
-    private int timeLeft                     = 4140000;//3600000;
+    private int timeLeft                      = 4140000;//3600000;
     private JLabel label                      = new JLabel("");
     private Timer timer                       = new Timer(1000, this);
     private JButton start                     = new JButton("Start");
@@ -30,8 +30,13 @@ class StressTester extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         timeLeft -= 1000;
-        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
-        label.setText(df.format(new Date(timeLeft)));
+        if (timeLeft >= 3600000) {
+            SimpleDateFormat df = new SimpleDateFormat("mm:ss");
+            label.setText("01:" + df.format(new Date(timeLeft)));
+        } else {
+            SimpleDateFormat df = new SimpleDateFormat("mm:ss");
+            label.setText("00:" + df.format(new Date(timeLeft)));
+        }
         if (timeLeft <= 0){
             start.setEnabled(true);
             stop.setEnabled(false);
