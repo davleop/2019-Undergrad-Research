bd034ad384b8c8dfca45f2f4362144fd98f0d06f
davleop <dlpenn1@buff.wtamu.edu>
2019-03-21 23:08:07 -0500

    Update java file

--- src/StressTester.java
+++ src/StressTester.java
@@ -14,6 +14,9 @@ import java.awt.event.*;
 import java.text.*;
 import java.util.concurrent.TimeUnit;
 
+// TODO (David): Make Image directory. Put Images in it. Fix Code.
+//               Go to bed zzz...
+
 class StressTester extends JFrame {
     private String path = "";
     
