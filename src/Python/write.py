e8d82540847f6acd1b6fa2da460b33d10aa69b68
davleop <dlpenn1@buff.wtamu.edu>
2019-04-08 22:38:59 -0500

    Optimize humanization

--- src/Python/write.py
+++ src/Python/write.py
@@ -1,3 +1,4 @@
+import humanize
 from cleanup import clean_files
 
 def write(filename, data):
@@ -6,18 +7,22 @@ def write(filename, data):
 	f.close()
 
 def wrapUp(): # TODO(David): Revise this method
-	q = float(open("a", 'r').readline().strip()[:-5])
-	w = float(open("b", 'r').readline().strip()[:-5])
-	e = float(open("c", 'r').readline().strip()[:-5])
-	r = float(open("d", 'r').readline().strip()[:-5])
-	t = float(open("e", 'r').readline().strip()[:-5])
+	q = float(open("a", 'r').readline().strip())
+	w = float(open("b", 'r').readline().strip())
+	e = float(open("c", 'r').readline().strip())
+	r = float(open("d", 'r').readline().strip())
+	t = float(open("e", 'r').readline().strip())
+
+	# TODO (David): make this the place it converts bytes per second to human readable
+	#				everywhere else, make it just calculate bytes per second. Then 
+	#				remove the import statements in the other files and just keep it here.
 
 	write_avg  = (q + w) / 2.0
 	append_avg = (e + r) / 2.0
 	read       = t
 
 	f = open("readme.txt", "w")
-	f.write(str(write_avg) + "\n" + str(append_avg) + "\n" + str(read))
+	f.write(humanize.naturalsize(write_avg) + "/s\n" + humanize.naturalsize(append_avg) + "/s\n" + humanize.naturalsize(read) + "/s")
 	f.close()
 
 	clean_files()
