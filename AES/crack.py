93d0bbeb17385c86b684874034b118d7c64483f1
davleop <dlpenn1@buff.wtamu.edu>
2019-04-11 16:37:28 -0500

    added start to detecting encryption standards

---
+++ AES/crack.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def detect_ecb_mode(plaintext, ciphertext):
+	pass
+
+def detect_cbc_mode(plaintext, ciphertext):
+	pass
+
+def detect_ofb_ctr_modes(plaintext, ciphertext):
+	pass # read more on the methods used...
+
+def detect_eax_gcm_modes(ciphertext):
+	pass # this ueses MAC. Just modify the file and try to decrypt...
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
