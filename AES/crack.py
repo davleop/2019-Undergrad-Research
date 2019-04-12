94236953074b31a330646cc5e899d4586e2727fa
davleop <dlpenn1@buff.wtamu.edu>
2019-04-12 12:26:31 -0500

    Update crack.py

--- AES/crack.py
+++ AES/crack.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-def detect_ecb_mode(plaintext, ciphertext):
-	pass
+def count_repetitions(ciphertext, block_size):
+	chunks = [ciphertext[i:i+block_size] for i in range(0, len(ciphertext), block_size)]
+	number_of_repetitions = len(chunks) - len(set(chunks))
+	result = {
+		'ciphertext': ciphertext,
+		'repetitions': number_of_repetitions
+	}
+	return result
+
+def detect_ecb_mode(ciphertext):
+	BLOCK_SIZE = 16
+	repetitions = [count_repetitions(cipher, BLOCK_SIZE) for cipher in ciphertext]
+
+	most_repetitions = sorted(repetitions, key=lambda x: x['repetitions'], reverse=True)[0]
+
+	print ("Ciphertext: {}".format(most_repetitions['ciphertext']))
+	print ("Repeating Blocks: {}". format(most_repetitions['repetitions']))
+
+	if most_repetitions['repetitions'] >= 3:
+		return True
+	return False
 
 def detect_cbc_mode(plaintext, ciphertext):
 	pass
@@ -13,7 +32,8 @@ def detect_eax_gcm_modes(ciphertext):
 	pass # this ueses MAC. Just modify the file and try to decrypt...
 
 def main():
-	pass
+	ciphertext = [bytes.fromhex(line.strip()) for line in open("8.txt")]
+	print(detect_ecb_mode(ciphertext))
 
 if __name__ == '__main__':
 	main()
